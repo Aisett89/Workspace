f99f3565796d85a95fc9425eff4a820f6c28c34c
Alexander Isett <alexander.isett@g.austincc.edu>
2021-11-03 17:30:36 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "dnd.h"
+
+int main()
+{
+    cout<<"Welcome to AHI's DND simulator!"<<endl;
+    cout<<"\nFirst let's pick your character. "<<endl;
+    cout<<"The two options of playstyle is magic or physical. In this game you may play either a wizard or a knight.";
+    return 0;
+}
